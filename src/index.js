7ed78ce12b97fd7a6589b3036595c357a2846176
AcelisWeaven <jeremy.graziani@gmail.com>
2021-11-12 18:55:55 +0100

    Change current year injection

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './scss/main.scss'
 import './js/app'
 import './js/icons'
+import './js/currentYear'
---
+++ src/js/currentYear.js
@@ -0,0 +1,1 @@
+document.querySelector('[data-current-year]').textContent = new Date().getFullYear()
